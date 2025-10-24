a77b5ebe09ad2eb858026f3786b6bdecacd27f63
JulianPatzner <Julian.Patzner@uk-halle.de>
2025-10-24 17:23:31 +0200

    set standard age function to age in days and added "age_in_years"

--- src/methods/agents.jl
+++ src/methods/agents.jl
@@ -1,28 +1,28 @@
-export age, age_in_days
+export age, age_in_years
 
 """
     age(individual::Individual, sim::Simulation)
 
-Calculates the age of an individual in completed years.
-"""
-function age(individual::Individual, sim::Simulation)::Int
-    return age_in_days(individual, sim) ÷ 365
-end
-
-function age(individual::Individual, date::Date)::Int
-    return age_in_days(individual, date) ÷ 365
-end
-
-"""
-    age_in_days(individual::Individual, sim::Simulation)
-
 Calculates the age of an individual in days on the current simulation tick.
 """
-function age_in_days(individual::Individual, sim::Simulation)::Int
+function age(individual::Individual, sim::Simulation)::Int
     current_date = sim.startdate + Day(tick(sim))
     return (current_date - individual.birthday).value
 end
 
-function age_in_days(individual::Individual, date::Date)::Int
+function age(individual::Individual, date::Date)::Int
     return (date - individual.birthday).value
+end
+
+"""
+    age_in_years(individual::Individual, sim::Simulation)
+
+Calculates the age of an individual in completed years.
+"""
+function age_in_years(individual::Individual, sim::Simulation)::Int
+    return age(individual, sim) ÷ 365
+end
+
+function age_in_years(individual::Individual, date::Date)::Int
+    return age(individual, date) ÷ 365
 end
