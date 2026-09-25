2390e8667596962ceb696ac34b57a05b3cd806f4
Julian-Patzner <Julian.Patzner@uk-halle.de>
2026-09-25 09:57:07 +0200

    age cohorts in age_incidence are now counted from the infectee

--- src/analysis/post_processing/pp_age_incidence.jl
+++ src/analysis/post_processing/pp_age_incidence.jl
@@ -54,8 +54,8 @@ function age_incidence(postProcessor::PostProcessor, timespan::Int64, basesize::
         pid = id(p)
         p_infs = subset(sim_infs, :pathogen_id => ByRow(==(pid)), view=true)
 
-        # one selection of :age_a for all cohorts; each selection copies the column in group order
-        cohort_counts = :age_a => (a -> NamedTuple(col => betweenage(a, lo, hi) for (col, lo, hi) in age_cohorts)) => AsTable
+        # one selection of :age_b for all cohorts; each selection copies the column in group order
+        cohort_counts = :age_b => (a -> NamedTuple(col => betweenage(a, lo, hi) for (col, lo, hi) in age_cohorts)) => AsTable
         coalesce_zero = [col => ByRow(x -> coalesce(x, 0)) => col for col in value_cols]
 
         incidence = groupby(p_infs, :tick) |>
